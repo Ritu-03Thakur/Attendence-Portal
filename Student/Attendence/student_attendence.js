2bfac5b314a36b5c9798f807814e07c30752a813
Yash Gaur <yashgaur2525@gmail.com>
2022-11-11 22:47:13 -0500

    added logic to subject buttons

---
+++ Student/Attendence/student_attendence.js
@@ -0,0 +1,94 @@
+const menu = document.getElementById('menu');
+const cross = document.getElementById('cross');
+const header = document.querySelector('header');
+
+menu.addEventListener('click', function(){
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('cross').style.display = 'block';
+    document.getElementById('header').style.marginLeft = '-350px';
+    // document.querySelector('nav').style.width = '70%';
+
+})
+cross.addEventListener('click', function(){
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('cross').style.display = 'none';
+    document.getElementById('header').style.marginLeft = '0px';
+    // header.classList.remove('active');
+
+})
+
+
+
+
+
+
+
+
+
+
+const overall = document.getElementById('overall_1');
+const cm = document.getElementById('cm');
+const ds = document.getElementById('ds');
+const dlcd = document.getElementById('dlcd');   
+const dm = document.getElementById('dm');
+const oops = document.getElementById('oops');
+
+overall_1.addEventListener('click' ,function(){
+    document.getElementById('overall_2').style.display = 'block';
+    document.getElementById('computational_methods').style.display = 'none';
+    document.getElementById('data_structures').style.display = 'none';
+    document.getElementById('digital_logic').style.display = 'none';
+    document.getElementById('discrete_mathematics').style.display = 'none';
+    document.getElementById('object_oriented').style.display = 'none';
+
+});
+cm.addEventListener('click' ,function(){
+    document.getElementById('overall_2').style.display = 'none';
+    document.getElementById('computational_methods').style.display = 'block';
+    document.getElementById('data_structures').style.display = 'none';
+    document.getElementById('digital_logic').style.display = 'none';
+    document.getElementById('discrete_mathematics').style.display = 'none';
+    document.getElementById('object_oriented').style.display = 'none';
+
+});
+ds.addEventListener('click' ,function(){
+    document.getElementById('overall_2').style.display = 'none';
+    document.getElementById('computational_methods').style.display = 'none';
+    document.getElementById('data_structures').style.display = 'block';
+    document.getElementById('digital_logic').style.display = 'none';
+    document.getElementById('discrete_mathematics').style.display = 'none';
+    document.getElementById('object_oriented').style.display = 'none';
+
+});
+dlcd.addEventListener('click' ,function(){
+    document.getElementById('overall_2').style.display = 'none';
+    document.getElementById('computational_methods').style.display = 'none';
+    document.getElementById('data_structures').style.display = 'none';
+    document.getElementById('digital_logic').style.display = 'block';
+    document.getElementById('discrete_mathematics').style.display = 'none';
+    document.getElementById('object_oriented').style.display = 'none';
+
+});
+
+dm.addEventListener('click' ,function(){
+    document.getElementById('overall_2').style.display = 'none';
+    document.getElementById('computational_methods').style.display = 'none';
+    document.getElementById('data_structures').style.display = 'none';
+    document.getElementById('digital_logic').style.display = 'none';
+    document.getElementById('discrete_mathematics').style.display = 'block';
+    document.getElementById('object_oriented').style.display = 'none';
+
+});
+oops.addEventListener('click' ,function(){
+    console.log('oops');
+    document.getElementById('overall_2').style.display = 'none';
+    document.getElementById('computational_methods').style.display = 'none';
+    document.getElementById('data_structures').style.display = 'none';
+    document.getElementById('digital_logic').style.display = 'none';
+    document.getElementById('discrete_mathematics').style.display = 'none';
+    document.getElementById('object_oriented').style.display = 'block';
+
+});
+
+
+
