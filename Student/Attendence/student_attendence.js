23999a4d5c6771de0a72c8b3f2747df3d2d9aaae
Yash Gaur <yashgaur2525@gmail.com>
2022-11-21 23:23:01 -0500

    improved sidebar and other responsiveness

--- Student/Attendence/student_attendence.js
+++ Student/Attendence/student_attendence.js
@@ -12,10 +12,8 @@ const oops = document.getElementById('oops');
 menu.addEventListener('click', function(){
     document.getElementById('menu').style.display = 'none';
     document.getElementById('cross').style.display = 'block';
-    document.getElementById('header').style.marginLeft = '-350px';
+    document.getElementById('header').style.marginLeft = '350px';
     document.getElementsByTagName('nav').style.marginLeft = '-100px';
-    document.getElementById('subjects').style.width = '100vw';
-    // document.querySelector('nav').style.width = '70%';
 
 })
 cross.addEventListener('click', function(){
